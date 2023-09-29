9066f1c5d519636d02ca4eab8cd7775f3b47a757
Murat Sankaya <muratsankaya2025@u.northwestern.edu>
2023-09-29 10:15:59 -0500

    implement selecting courses / update 1

--- src/Course.jsx
+++ src/Course.jsx
@@ -4,8 +4,6 @@ import './Course.css'
 const Course = ({courseCode, course, selected, toggleSelected}) => (
     <div key={courseCode} className={`card ${selected.includes(courseCode) ? 'selected' : ''}`} 
     onClick={() => toggleSelected(courseCode)}>
-    {/* //<div key={courseCode} className='card'
-    // onClick={() => toggleSelected(courseCode)}> */}
         <h5 className="card-title">{`${course.term} CS ${course.number}`}</h5>
         <div className='card-body'>
             <p className="card-text">{course.title}</p>
